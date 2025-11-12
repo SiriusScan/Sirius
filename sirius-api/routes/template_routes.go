60a674b1c096d992deb18d1d6a5d72f028d69b19
0sm0s1z <mtoussain@gmail.com>
2025-11-12 12:00:39 -0800

    fix(demo): add missing route setter files for API build

---
+++ sirius-api/routes/template_routes.go
@@ -0,0 +1,12 @@
+package routes
+
+import "github.com/gofiber/fiber/v2"
+
+// TemplateRouteSetter sets up template routes
+type TemplateRouteSetter struct{}
+
+func (t *TemplateRouteSetter) SetupRoutes(app *fiber.App) {
+	// Template routes will be implemented here
+	// Placeholder to satisfy RouteSetter interface
+}
+
